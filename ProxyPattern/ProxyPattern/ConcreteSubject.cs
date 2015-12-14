ae26a8b6beeea3c9e92e756ddff281af6731951e
yuandong <yuandong618@gmail.com>
2015-12-14 20:54:12 +0800

    add proxy pattern

---
+++ ProxyPattern/ProxyPattern/ConcreteSubject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern {
+	public class ConcreteSubject :Subject{
+		public override void Request()
+		{
+			Console.WriteLine("具体对象的请求");
+		}
+	}
+}
---
+++ ProxyPattern/ProxyPattern/Proxy.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyPattern {
+
+	//代理类和具体的实体都要遵循同一套接口
+	public  class Proxy:Subject
+	{
+		private ConcreteSubject subject ;
+		public override void Request()
+		{
+			if (subject == null)
+			{
+				subject = new ConcreteSubject();
+			}
+
+			subject.Request();
+		}
+	}
+}
